890217c55c327b5b790417a5bdab2279a55e9fcb
Trung Tran <trannguyenbaotrung@gmail.com>
2022-11-09 04:30:00 +0700

    Impl client connected/disconnected event

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 const TAG = require('path').basename(__filename);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, World!</h1>');
+    const file = __dirname + '/index.html';
+    LogUtil.log(TAG, file)
+    res.sendFile(file);
 });
+
+io.on('connection', (socket) => {
+    LogUtil.log(TAG, 'a user connected');
+    socket.on('disconnect', () => {
+        LogUtil.log(TAG, 'a user disconnected');
+    })
+})
 
 server.listen(Config.PORT, () => {
     LogUtil.log(TAG, 'Listening on port: ' + Config.PORT);
