3b1537888b0947a700a42598a2350b9e388d08a6
Trung Tran <trannguyenbaotrung@gmail.com>
2022-11-14 13:05:22 +0700

    Add Hilt DI

--- mobile_client/app/src/main/java/com/trungtran/androidchat/MainActivityCompose.kt
+++ mobile_client/app/src/main/java/com/trungtran/androidchat/MainActivityCompose.kt
@@ -1,16 +1,16 @@
 package com.trungtran.androidchat
 
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,7 +18,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.trungtran.androidchat.comm.SocketIO
 import com.trungtran.androidchat.ui.theme.AndroidChatTheme
 
 class MainActivityCompose : ComponentActivity() {
@@ -34,25 +36,19 @@ class MainActivityCompose : ComponentActivity() {
             }
         }
 
-//        val myWebView: WebView = findViewById(R.id.webview)
-//        myWebView.webViewClient = WebViewClient()
-//        myWebView.webChromeClient = WebChromeClient()
-//        myWebView.settings.javaScriptEnabled = true
-//        myWebView.loadUrl(Constant.CHAT_SERVER_URI)
-////        myWebView.loadUrl("https://youtube.com")
-//
-//        SocketIO.setSocket()
-//        SocketIO.connect()
-//
-//        val mSocket = SocketIO.getSocket()
-//        mSocket.emit("Hi from mobile!")
-//
-//        mSocket.on("chat message") {
-//            if (it[0] != null) {
-//                val msg = it[0] as String
-//                Log.d(MainActivityCompose.TAG, "received: $msg")
-//            }
-//        }
+
+        SocketIO.setSocket()
+        SocketIO.connect()
+
+        val mSocket = SocketIO.getSocket()
+        mSocket.emit("Hi from mobile!")
+
+        mSocket.on("chat message") {
+            if (it[0] != null) {
+                val msg = it[0] as String
+                Log.d(MainActivityCompose.TAG, "received: $msg")
+            }
+        }
     }
 
     companion object {
@@ -66,7 +62,7 @@ fun MultiChat() {
         modifier = Modifier
             .fillMaxHeight()
             .fillMaxWidth()
-            .background(Color.Yellow)
+//            .background(Color.Yellow)
     ) {
         Box(
             modifier = Modifier
@@ -77,11 +73,14 @@ fun MultiChat() {
             NativeChat()
         }
 
+        // Add a horizontal space between the img and the column
+        Spacer(modifier = Modifier.width((8.dp)))
+
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1.0f)
-                .background(Color.Red)
+//                .background(Color.Red)
         ) {
             Box(modifier = Modifier.align(Alignment.BottomStart)) {
                 WebChat()
@@ -92,8 +91,26 @@ fun MultiChat() {
 
 @Composable
 fun NativeChat() {
-    Text("NativeChat1")
-    Text("NativeChat2")
+    val messages = mutableListOf<String>()
+    for (i in 1..50) {
+        messages.add("msg $i")
+    }
+
+    Messages(messages)
+}
+
+@Composable
+fun Messages(messages: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(messages) {
+            message -> Message(message)
+        }
+    }
+}
+
+@Composable
+fun Message(message: String, modifier: Modifier = Modifier) {
+    Text(message)
 }
 
 @Composable
