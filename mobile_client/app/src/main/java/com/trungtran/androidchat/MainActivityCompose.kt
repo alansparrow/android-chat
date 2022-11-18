40fad2ec41d22a029cd85531b2e83fe9e114049e
Trung Tran <trannguyenbaotrung@gmail.com>
2022-11-18 08:09:59 +0700

    Polish

--- mobile_client/app/src/main/java/com/trungtran/androidchat/MainActivityCompose.kt
+++ mobile_client/app/src/main/java/com/trungtran/androidchat/MainActivityCompose.kt
@@ -7,19 +7,21 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.trungtran.androidchat.comm.SocketIO
 import com.trungtran.androidchat.ui.theme.AndroidChatTheme
@@ -45,7 +47,6 @@ class MainActivityCompose : ComponentActivity() {
             }
         }
 
-        mSocket.emit("chat message", "Hi from mobile!")
         mSocket.on("chat message") {
             if (it[0] != null) {
                 val msg = it[0] as String
@@ -65,8 +66,13 @@ fun MultiChat(msgs: List<String>) {
     Column(
         modifier = Modifier
             .fillMaxHeight()
-            .fillMaxWidth()
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = "Native Client",
+            fontSize = 20.sp
+        )
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -75,9 +81,10 @@ fun MultiChat(msgs: List<String>) {
             NativeChat(msgs)
         }
 
-        // Add a horizontal space between the img and the column
-        Spacer(modifier = Modifier.width((8.dp)))
-
+        Text(
+            text = "Webview Client",
+            fontSize = 20.sp
+        )
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -111,29 +118,32 @@ fun NativeChat(msgs: List<String>) {
                 .fillMaxWidth()
                 .weight(3f)
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
-            ) {
-                var text by remember { mutableStateOf("") }
-                TextField(
-                    value = text,
-                    onValueChange = { text = it },
-                    modifier = Modifier
-                        .padding(start = 5.dp, bottom = 5.dp)
-
-                )
-                Button(
-                    onClick = {
-                        SocketIO.getSocket().emit("chat message", text)
-                        text = ""
-                    }, modifier = Modifier
-                        .padding(5.dp)
-                ) {
-                    Text(text = "Send")
+            var text by remember { mutableStateOf("") }
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                placeholder = {
+                    Text("Enter message")
+                },
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                shape = RoundedCornerShape(25.dp),
+                trailingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_send_msg1),
+                        contentDescription = null,
+                        tint = MaterialTheme.colors.primary,
+                        modifier = Modifier.clickable {
+                            SocketIO.getSocket().emit("chat message", text)
+                            text = ""
+                        }
+                    )
                 }
-            }
 
+            )
         }
     }
 
@@ -150,7 +160,9 @@ fun Messages(messages: List<String>, modifier: Modifier = Modifier) {
     }
 
     LaunchedEffect(key1 = messages.size) {
-        listState.animateScrollToItem(messages.size - 1)
+        if (messages.size - 1 >= 0) {
+            listState.animateScrollToItem(messages.size - 1)
+        }
     }
 }
 
